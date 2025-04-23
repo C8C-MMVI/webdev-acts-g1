ab54f19386a9e802cb6b88f73f343eb9d1f52a1d
C8C-MMVI <raivenchristian.navor@lorma.edu>
2025-04-23 14:53:52 +0800

    Some commits and added basic calculator with JS

---
+++ intro/Finals/js/script.js
@@ -0,0 +1,67 @@
+const result = document.getElementById("result")
+
+document.getElementById("add").addEventListener("click", function(){
+    let num1 = parseFloat(document.getElementById("operand1").value)
+    let num2 = parseFloat(document.getElementById("operand2").value)
+    
+    if(isNaN(num1) || isNaN(num2)){
+        result.textContent = "Please enter valid numbers"
+        result.classList.add("error")
+        return
+    }
+
+    result.classList.remove("error")
+    result.classList.add("success")
+    result.textContent = "The result is " + (num1 + num2)
+})
+
+document.getElementById("add").addEventListener("click", function(){
+    let num1 = parseFloat(document.getElementById("operand1").value)
+    let num2 = parseFloat(document.getElementById("operand2").value)
+    
+    if(isNaN(num1) || isNaN(num2)){
+        result.textContent = "Please enter valid numbers"
+        result.classList.add("error")
+        return
+    }
+
+    result.classList.remove("error")
+    result.classList.add("success")
+    result.textContent = "The result is " + (num1 - num2)
+})
+
+document.getElementById("multiply").addEventListener("click", function(){
+    let num1 = parseFloat(document.getElementById("operand1").value)
+    let num2 = parseFloat(document.getElementById("operand2").value)
+    
+    if(isNaN(num1) || isNaN(num2)){
+        result.textContent = "Please enter valid numbers"
+        result.classList.add("error")
+        return
+    }
+
+    result.classList.remove("error")
+    result.classList.add("success")
+    result.textContent = "The result is " + (num1 * num2)
+})
+
+document.getElementById("divide").addEventListener("click", function(){
+    let num1 = parseFloat(document.getElementById("operand1").value)
+    let num2 = parseFloat(document.getElementById("operand2").value)
+    
+    if(isNaN(num1) || isNaN(num2)){
+        result.textContent = "Please enter valid numbers"
+        result.classList.add("error")
+        return
+    }
+
+    result.classList.remove("error")
+
+    if (num2 === 0){
+        result.textContent = "Cannot divide by 0"
+        result.classList.add("error")
+        return
+    }
+    result.classList.add("success")
+    result.textContent = "The result is " + (num1 / num2)
+})
